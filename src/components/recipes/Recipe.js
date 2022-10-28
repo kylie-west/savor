1a8818a0db93f3f3a56d9e14af50ece937195fbf
Kylie West <kmichellewest@gmail.com>
2022-10-27 17:23:23 -0700

    Update dashboard/recipe layout and sizing, background images

--- src/components/recipes/Recipe.js
+++ src/components/recipes/Recipe.js
@@ -3,18 +3,20 @@ import React from "react";
 function Recipe() {
 	return (
 		<article className="recipe">
-			<section>
-				<h1 className="recipe-title">Title</h1>
-				<p className="recipe-description">Description</p>
+			<section className="recipe-title-section">
+				<h1>Title</h1>
+				<p>Description</p>
 			</section>
 
 			<section>
 				<h2>Ingredients</h2>
+				<hr />
 				<ul>List items go here</ul>
 			</section>
 
 			<section>
 				<h2>Directions</h2>
+				<hr />
 				<ol>List items go here</ol>
 			</section>
 		</article>
