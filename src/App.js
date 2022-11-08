2447cd8be8269dda3d7e28181405b346d5bacf83
Kylie West <kmichellewest@gmail.com>
2022-11-08 10:20:44 -0700

    Set up React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import useFirebaseAuth from "./hooks/useFirebaseAuth";
+import { Route, Routes } from "react-router-dom";
 import RecipeContextProvider from "./context/recipeContext";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
 	const user = useFirebaseAuth();
 
 	return (
 		<RecipeContextProvider>
-			<div className="App">
-				<Dashboard user={user} />
-			</div>
+			<Routes>
+				<Route path="/" element={<Dashboard user={user} />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+				{/* TODO: 404 page */}
+			</Routes>
 		</RecipeContextProvider>
 	);
 }
